d58a1899eea476b842066256c595c6578c098ae2
Rachit <rachitpatni028@gmail.com>
2022-12-09 11:28:24 +0530

    fixed typos

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def get_all_customers():
     with sql.connect('supermarket.db') as connect:
         cur=connect.cursor()
         data=cur.execute("SELECT * FROM Customers")
-        dict_data=map(lambda x:{'CUSTOMER_ID':x[0],'FIRST_NAME':x[1],'LAST_NAME':x[2],'GENDER':x[3]},data)
+        dict_data=map(lambda x:{'customer_id':x[0],'first_name':x[1],'last_name':x[2],'gender':x[3]},data)
     return jsonify(list(dict_data))
 
 
@@ -56,13 +56,12 @@ def get_all_invoices():
         )
 
 
-
 @app.get('/stock')
 def get_stock():
     with sql.connect('supermarket.db') as connect:
         cur=connect.cursor()
         data=cur.execute("SELECT * FROM Stock")
-        dict_data=map(lambda x:{'PRODUCT_ID':x[0],'STORE_ID':x[1],'AVAIABLE_STOCK':x[2]},data)
+        dict_data=map(lambda x:{'product_id':x[0],'store_id':x[1],'available_stock':x[2]},data)
     return jsonify(list(dict_data))
 
 
